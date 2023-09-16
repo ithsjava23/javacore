914bc46154d24aa683fbad0178a9fbb2847a5232
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-16 20:41:49 +0200

    Add tests for Warehouse

--- src/test/java/org/example/WarehouseTest.java
+++ src/test/java/org/example/WarehouseTest.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.*;
 
 import java.lang.reflect.Constructor;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @DisplayName("A warehouse")
@@ -21,6 +26,7 @@ class WarehouseTest {
     @Test
     @DisplayName("should have no public constructors")
     @Order(1)
+    @Tag("basic")
     void shouldHaveNoPublicConstructors() {
         Class<Warehouse> clazz = Warehouse.class;
         Constructor<?>[] constructors = clazz.getConstructors();
@@ -30,6 +36,7 @@ class WarehouseTest {
     @Test
     @DisplayName("is created by calling createWarehouse")
     @Order(2)
+    @Tag("basic")
     void isCreatedWithFactory() {
         Warehouse warehouse = Warehouse.getInstance();
         assertThat(warehouse).isNotNull();
@@ -38,6 +45,7 @@ class WarehouseTest {
     @Test
     @DisplayName("can be created with a name")
     @Order(3)
+    @Tag("basic")
     void canBeCreatedUsingAName() {
         Warehouse warehouse = Warehouse.getInstance("MyStore");
         assertThat(warehouse).isNotNull().extracting("name").isEqualTo("MyStore");
@@ -46,6 +54,7 @@ class WarehouseTest {
     @Test
     @DisplayName("should be the same instance when using the same name")
     @Order(4)
+    @Tag("basic")
     void shouldBeSameInstanceForSameName() {
         Warehouse warehouse1 = Warehouse.getInstance("Just a name");
         Warehouse warehouse2 = Warehouse.getInstance("Just a name");
@@ -60,7 +69,7 @@ class WarehouseTest {
     class WhenNew {
 
         @BeforeEach
-        void createWarehouse(){
+        void createWarehouse() {
             warehouse = Warehouse.getInstance("New warehouse");
         }
 
@@ -73,7 +82,7 @@ class WarehouseTest {
         @Test
         @DisplayName("returns empty list of products")
         void returnsEmptyListOfProducts() {
-            assertThat(warehouse.getAllProducts()).isEmpty();
+            assertThat(warehouse.getProducts()).isEmpty();
         }
 
         @Nested
@@ -81,21 +90,120 @@ class WarehouseTest {
         class AfterAddingProduct {
 
             ProductRecord addedProduct;
+            String UUID_name = "5fc03087-d265-11e7-b8c6-83e29cd24f4c";
 
             @BeforeEach
             void addingAProduct() {
-                addedProduct = warehouse.addProduct(UUID.randomUUID(),"Milk", Category.of("Dairy"), BigDecimal.valueOf(999,100));
+                addedProduct = warehouse.addProduct(UUID.randomUUID(), "Milk", Category.of("Dairy"), BigDecimal.valueOf(999, 2));
             }
 
             @Test
-            @DisplayName("getAllProducts should return list with one product")
-            void getAllShouldReturnListWithOneProduct() {
-                assertThat(warehouse.getAllProducts()).containsExactly(new ProductRecord(UUID.randomUUID(),"Milk", Category.of("Dairy"), BigDecimal.valueOf(999,100)));
+            @DisplayName("it is no longer empty")
+            void itIsNoLongerEmpty() {
+                assertThat(warehouse.isEmpty()).isFalse();
             }
+
+            @Test
+            @DisplayName("returns list with that product")
+            void getAllShouldReturnListWithOneProduct() {
+                assertThat(warehouse.getProducts()).containsExactly(addedProduct);
+            }
+
+            @Test
+            @DisplayName("valid id returns product")
+            void getProductByIdShouldReturnProductWithThatId() {
+                assertThat(warehouse.getProductById(addedProduct.uuid())).contains(addedProduct);
+            }
+
+            @Test
+            @DisplayName("invalid id returns empty")
+            void getSingleProductWithInvalidIdShouldBeEmpty() {
+                assertThat(warehouse.getProductById(UUID.fromString(UUID_name))).isEmpty();
+            }
+
+            @Test
+            @DisplayName("throws IllegalArgumentException when using existing id")
+            void shouldThrowExceptionIfTryingToAddProductWithSameId() {
+                assertThatThrownBy(() ->
+                        warehouse.addProduct(UUID.randomUUID(), "Milk", Category.of("Dairy"), BigDecimal.valueOf(999, 2)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Product with that id already exists, use updateProduct for updates.");
+            }
+        }
+
+        @Nested
+        @DisplayName("after adding multiple products")
+        class AfterAddingMultipleProducts {
+            List<ProductRecord> addedProducts = new ArrayList<>();
+            String UUID_name = "5fc03087-d265-11e7-b8c6-83e29cd24f4c";
+
+            @BeforeEach
+            void addingAProduct() {
+                addedProducts.add(warehouse.addProduct(UUID.randomUUID(), "Milk", Category.of("Dairy"), BigDecimal.valueOf(999, 2)));
+                addedProducts.add(warehouse.addProduct(UUID.randomUUID(), "Apple", Category.of("Fruit"), BigDecimal.valueOf(290, 2)));
+                addedProducts.add(warehouse.addProduct(UUID.randomUUID(), "Bacon", Category.of("Meat"), BigDecimal.valueOf(1567, 2)));
+            }
+
+            @Test
+            @DisplayName("returns list with all products")
+            void returnsListWithAllProducts() {
+                assertThat(warehouse.getProducts()).isEqualTo(addedProducts);
+            }
+
+            @Test
+            @DisplayName("changing a products price should be saved")
+            void changingAProductsNameShouldBeSaved() {
+                warehouse.updateProductPrice(addedProducts.get(1).uuid(), BigDecimal.valueOf(311, 2));
+                assertThat(warehouse.getProductById(addedProducts.get(1).uuid())).isNotEmpty()
+                        .get()
+                        .hasFieldOrPropertyWithValue("price", BigDecimal.valueOf(311, 2));
+            }
+
+            @Test
+            @DisplayName("get a map with all products for each category")
+            void getAMapWithAllProductsForEachCategory() {
+                Map<Category, List<ProductRecord>> productsOfCategories =
+                        Map.of(addedProducts.get(0).category(), List.of(addedProducts.get(0)),
+                                addedProducts.get(1).category(), List.of(addedProducts.get(1)),
+                                addedProducts.get(2).category(), List.of(addedProducts.get(2)));
+                assertThat(warehouse.getProductsGroupedByCategories()).isEqualTo(productsOfCategories);
+            }
+
+            @Test
+            @DisplayName("list returned from getProducts should be unmodifiable")
+            void listReturnedFromGetProductsShouldBeImmutable() {
+                var products = warehouse.getProducts();
+                assertThatThrownBy(() -> products.remove(0))
+                        .isInstanceOf(UnsupportedOperationException.class);
+            }
+
+            @Test
+            @DisplayName("throws IllegalArgumentException when trying to change an invalid id")
+            void throwsIllegalArgumentExceptionWhenTryingToChangeAnInvalidId() {
+                assertThatThrownBy(() ->
+                        warehouse.updateProductPrice(UUID.fromString("9e120341-627f-32be-8393-58b5d655b751"), BigDecimal.valueOf(1000, 2)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Product with that id doesn't exist.");
+            }
+
+            @Test
+            @DisplayName("find a product belonging to a category")
+            void findProductsBelongingToACategory() {
+                assertThat(warehouse.getProductsBy(Category.of("Meat")))
+                        .containsOnly(addedProducts.get(2));
+            }
+
+            @Test
+            @DisplayName("find multiple products from same category")
+            void findMultipleProductsFromSameCategory() {
+                addedProducts.add(warehouse.addProduct(UUID.randomUUID(), "Steak", Category.of("Meat"), BigDecimal.valueOf(399, 0)));
+                assertThat(warehouse.getProductsBy(Category.of("Meat")))
+                        .containsOnly(addedProducts.get(2), addedProducts.get(3));
+            }
+
 
         }
     }
-
 
 
 //
